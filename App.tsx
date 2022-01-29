680393bb72a2094c8d523a3c176554dba9ce7755
libero <lneto@tecnologia-unica.com.br>
2022-01-29 13:03:01 -0400

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import {Home} from './src/pages/Home';
+
+export function App() {
+  return (
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" />
+    </>
+  );
+}
